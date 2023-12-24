a92ea49bf427cbf8ba2b1ac9742a548a0607f93a
PedroMoT4 <pedroheriquecarvalhomota@gmail.com>
2023-12-24 14:33:16 -0300

    Updating bee_3048 with the right solution

--- bee_3048.c
+++ bee_3048.c
@@ -2,7 +2,7 @@
 
 int main(){
 
-    int n;
+    int n, i , j;
     scanf("%d", &n);
 
     int seq[n];
@@ -13,10 +13,27 @@ int main(){
 
     int count = 0;
 
-    for(int j = 0; j < n; j++){
-        if(seq[j] != seq[j+1]){
-            count++;
+    int a = seq[0];
+
+    for(i = 0; i < n; i++){
+        if(a == 1){
+            for(j = i; j < n; j++){
+                if(seq[j] == 2){
+                    break;
+                }
+                a = 2;
+                i = j;
+            }
+        }else{
+            for(j = i; j < n; j++){
+                if(seq[j] == 1){
+                    break;
+                }
+                a = 1;
+                i = j;
+            }
         }
+        count++;
     }
 
     printf("%d\n", count);
