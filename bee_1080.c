646d702a0c77e04dbc0c913b31ab31fddd1ab335
Pedro Mota <pedroheriquecarvalhomota@gmail.com>
2024-09-07 12:39:03 -0500

    Beecrowd 9/7/24

---
+++ bee_1080.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int n[100];
+    int max = 0, pos = 0;
+
+    for(int i = 0; i < 100; i++){
+        scanf("%d", &n[i]);
+        if(n[i] > max){
+            max = n[i];
+            pos = i+1;
+        }
+    }
+
+    printf("%d\n", max);
+    printf("%d\n", pos);
+
+    return 0;
+}
